3f54cf1ed0e53fa20116337cf05b0f27b8ddcd33
hassan <hassanakranprog@gmail.com>
2020-08-11 15:10:48 +0300

    8-11-2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quotes/ui/pages/quote_screen.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+
 final DBRef = FirebaseDatabase.instance.reference();
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,9 +19,6 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: QuoteScreen(),
       ),
-
     );
   }
-
-
 }
--- lib/ui/pages/quote_screen.dart
+++ lib/ui/pages/quote_screen.dart
@@ -1,14 +1,71 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_quotes/ui/common/quote_widget.dart';
 import 'package:flutter_screenutil/screenutil.dart';
 import 'package:random_color/random_color.dart';
 
-final Firestore _firestore = Firestore();
-final RandomColor _randomColor = RandomColor();
+class _LoadingIndicator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
 
-class QuoteScreen extends StatelessWidget {
+class QuoteScreen extends StatefulWidget {
+  @override
+  _QuoteScreenState createState() => _QuoteScreenState();
+}
 
+class _QuoteScreenState extends State<QuoteScreen> {
+  final Firestore _db = Firestore.instance;
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+  final Firestore _firestore = Firestore();
+  final RandomColor _randomColor = RandomColor();
+  _getToken() {
+    _fcm.getToken().then((token) {
+      _db
+          .collection("tokens")
+          .add({"token": token}).then((value) => print(value));
+    });
+  }
+
+  @override
+  void initState() {
+    _fcm.requestNotificationPermissions(IosNotificationSettings());
+      _getToken();
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        // showDialog(
+        //     context: context,
+        //     builder: (context) => AlertDialog(
+        //             content: ListTile(
+        //             title: Text(message['notification']['title']),
+        //             subtitle: Text(message['notification']['body']),
+        //             ),
+        //             actions: <Widget>[
+        //             FlatButton(
+        //                 child: Text('Ok'),
+        //                 onPressed: () => Navigator.of(context).pop(),
+        //             ),
+        //         ],
+        //     ),
+        // );
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        // _getToken();
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        
+        // TODO optional
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,15 +91,6 @@ class QuoteScreen extends StatelessWidget {
           );
         },
       ),
-    );
-  }
-}
-
-class _LoadingIndicator extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: CircularProgressIndicator(),
     );
   }
 }
